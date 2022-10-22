bbc8391989dbd3a232b4c1bac98985a05d7d58b8
Niklas Schulte <niklas.schulte@rwth-aachen.de>
2022-10-22 11:03:55 +0200

    Setup team

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # usage: deploy.sh
 date=$(date +%s)
-helm upgrade --install --namespace team-TEAM_NAME --values "deployment/values.gcloud.yaml" --set metadata.date="$date" solution deployment/chart
+helm upgrade --install --namespace team-aixtra --values "deployment/values.gcloud.yaml" --set metadata.date="$date" solution deployment/chart
--- set_team_name.sh
+++ set_team_name.sh
@@ -4,8 +4,8 @@
 TEAM_NAME="$1"
 CLIENT_SECRET="$2"
 
-sed -i "s/TEAM_NAME/$TEAM_NAME/g" ./pom.xml
-sed -i "s/TEAM_NAME/$TEAM_NAME/g" ./deploy.sh
-sed -i "s/CLIENT_ID/$TEAM_NAME/g" ./deployment/values.gcloud.yaml
-sed -i "s/TEAM_NAME/$TEAM_NAME/g" ./deployment/values.gcloud.yaml
-sed -i "s/CLIENT_SECRET/$CLIENT_SECRET/g" ./deployment/values.gcloud.yaml
+sed -i "" "s/TEAM_NAME/$TEAM_NAME/g" ./pom.xml
+sed -i "" "s/TEAM_NAME/$TEAM_NAME/g" ./deploy.sh
+sed -i "" "s/CLIENT_ID/$TEAM_NAME/g" ./deployment/values.gcloud.yaml
+sed -i "" "s/TEAM_NAME/$TEAM_NAME/g" ./deployment/values.gcloud.yaml
+sed -i "" "s/CLIENT_SECRET/$CLIENT_SECRET/g" ./deployment/values.gcloud.yaml
